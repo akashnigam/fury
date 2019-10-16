a50a275d96b8e9cfb449fe27b5818959eaf88456
Akash Nigam <anigam6@asu.edu>
2019-10-16 03:25:21 +0000

    updated code

--- first.py
+++ first.py
@@ -1,6 +1,20 @@
 from swpag_client import Team
 import json
-t = Team("http://actf0.cse545.rev.fish/", "IeaL1xdIryga0Ubazn2Zi2Sh3Gf47RdN")
+
+def netcat(hostname, port, content):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((hostname, port))
+    s.sendall(content)
+    s.shutdown(socket.SHUT_WR)
+    while 1:
+        data = s.recv(1024)
+        if data == "":
+            break
+        print "Received:", repr(data)
+    print "Connection closed."
+    s.close()
+
+t = Team("http://actf0.cse545.rev.fish/", "lpmrUtF4wT1mu5FnVN6Tt82LnK1j9n5d")
 print(t.game_url)
 #print(t.get_vm())
 print(t.get_game_status())
@@ -9,11 +23,12 @@ print(t.get_service_list())
 services = t.get_service_list()
 print('services:',services)
 print()
+print()
 for service in services:
     print('SERVICE NAME:',service)
-    #serviceObj = json.loads(service)
-    targets = t.get_targets(service['service_id'])
-    #targets = t.get_targets(10001)
+    print('SERVICE ID:',service['service_id'])
+    service_id = service['service_id']
+    targets = t.get_targets(service_id)
     for target in targets:
-        print('TARGET NAME:',target)
-
+        print('TARGET NAME:',target)#,netcat(target,''))
+	print(netcat(target))
